6796c21fd1a3a892e3ccadc07345ddb0add1d266
Peter Asp <peter.aspus@gmail.com>
2017-09-17 20:34:55 +0200

    HA-428

--- CI-SeleniumJenkins/src/testcases/TestCase_002.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_002.java
@@ -11,7 +11,7 @@ public class TestCase_002 {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
 		driver.get("http://facebook.com");
-		driver.findElementById("email").sendKeys("Hello");
+		driver.findElementById("email").sendKeys("Hi");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
 		
