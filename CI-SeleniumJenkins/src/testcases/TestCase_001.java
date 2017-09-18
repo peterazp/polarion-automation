be6e94cbcb7cdb3f995c40d92bf6adcbf74fb3f7
Peter Asp <peter.aspus@gmail.com>
2017-09-18 08:26:07 +0200

    Adding Resource traceability. HA-430

--- CI-SeleniumJenkins/src/testcases/TestCase_001.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_001.java
@@ -10,6 +10,9 @@ public class TestCase_001 {
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
+		/**
+		 * @wi.implements HomeAutomation2000/HA-430 It should say facebook
+		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
