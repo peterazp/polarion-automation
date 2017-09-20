f440db80f71d711179f5cad0bd0f26bba2b3fce9
Peter Asp <peter.aspus@gmail.com>
2017-09-20 08:45:49 +0200

    HA-440 Fine-tuning TestCase_002

--- CI-SeleniumJenkins/src/testcases/TestCase_002.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_002.java
@@ -10,6 +10,9 @@ public class TestCase_002 {
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
+		/**
+		 * @wi.implements HomeAutomation2000/HA-440 It should say facebook
+		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hola");
 		driver.findElementById("pass").sendKeys("Hello");
