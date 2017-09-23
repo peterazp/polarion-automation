e403d1380300179e3ba7620bfd957b811d143d8a
Peter Asp <peter.aspus@gmail.com>
2017-09-23 23:12:52 +0200

    Changed stuff

--- CI-SeleniumJenkins/src/testcases/TestCase_002.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_002.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase_002 {
-
+// Updating source code
 	@Test
 	public void testcase1()
 	{
@@ -14,7 +14,7 @@ public class TestCase_002 {
 		 * @wi.implements HomeAutomation2000/HA-440 It should say facebook
 		 */
 		driver.get("http://facebook.com");
-		driver.findElementById("email").sendKeys("Hej");
+		driver.findElementById("email").sendKeys("H");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
 		
