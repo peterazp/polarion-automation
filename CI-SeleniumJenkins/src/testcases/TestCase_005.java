67272a4c4abb1afc31a56d273757be10544ec6cd
Peter Asp <peter.aspus@gmail.com>
2017-09-24 00:05:44 +0200

    dfdsfdsf

--- CI-SeleniumJenkins/src/testcases/TestCase_005.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_005.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase_005 {
-// Adding comment here....
+// Adding comment home....
 	@Test
 	public void testcase1()
 	{
