47314a8ad0211caef56a8d6fbcd2394722777dbf
Peter Asp <peter.aspus@gmail.com>
2017-09-24 08:21:06 +0200

    HA-536 Refining the test case

--- CI-SeleniumJenkins/src/testcases/TestCase_002.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_002.java
@@ -4,17 +4,17 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase_002 {
-// Updating source code
+	
 	@Test
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
 		/**
-		 * @wi.implements HomeAutomation2000/HA-440 It should say facebook
+		 * @wi.implements HomeAutomation2000/HA-536 Implementing chrome driver
 		 */
 		driver.get("http://facebook.com");
-		driver.findElementById("email").sendKeys("H");
+		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
 		
--- CI-SeleniumJenkins/src/testcases/TestCase_003.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_003.java
@@ -4,12 +4,12 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase_003 {
-
+	// Changing code by adding a comment
 	@Test
 	public void testcase1()
 	{
 		/**
-		 * @wi.implements HomeAutomation2000/HA-430 Testing RT
+		 * @wi.implements HomeAutomation2000/HA-536 Testing RT
 		 */
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
