0194b4e734fdec152d06fdf160a2b280475f2f24
Peter Asp <peter.aspus@gmail.com>
2017-09-24 16:43:51 +0200

    HA-550 Implementing Function A

--- CI-SeleniumJenkins/src/testcases/TestCase_002.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_002.java
@@ -10,13 +10,12 @@ public class TestCase_002 {
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
-		/**
-		 * @wi.implements HomeAutomation2000/HA-536 Implementing Chrome driver
-		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
 		driver.findElementById("pass").sendKeys("Hello");
 		driver.quit();
-		
+		/**
+		 * @wi.implements HomeAutomation2000/HA-536 Implementing Funtion A
+		 */
 	}
 }
