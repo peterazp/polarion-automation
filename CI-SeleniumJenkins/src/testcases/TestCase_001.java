6b3e7877323373121ebc16d3d84e1696a7c6a966
Peter Asp <peter.aspus@gmail.com>
2017-09-25 20:25:24 +0200

    HA-550 Implementing Function A, again

--- CI-SeleniumJenkins/src/testcases/TestCase_001.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_001.java
@@ -11,7 +11,7 @@ public class TestCase_001 {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
 		/**
-		 * @wi.implements HomeAutomation2000/HA-430 It should say facebook
+		 * @wi.implements HomeAutomation2000/HA-550 Implementing Function A
 		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
