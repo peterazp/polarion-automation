e489ed7b2887ff8bcbcdfca455cfdb370a2695d9
Peter Asp <peter.aspus@gmail.com>
2017-09-28 20:05:52 +0200

    HA-560 testing

--- CI-SeleniumJenkins/src/testcases/TestCase_001.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_001.java
@@ -4,14 +4,14 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase_001 {
-// Add new comment here
+// Add new comment here or here
 	@Test
 	public void testcase1()
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
 		/**
-		 * @wi.implements HomeAutomation2000/HA-550 Implementing Function A
+		 * @wi.implements HomeAutomation2000/HA-560 Implementing Function A
 		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
