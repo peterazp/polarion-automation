b402349342ec903aab45458948851593fa3741fa
Peter Asp <peter.aspus@gmail.com>
2017-09-29 08:52:04 +0200

    hhhhhhhhhh

--- CI-SeleniumJenkins/src/testcases/TestCase_005.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_005.java
@@ -9,7 +9,7 @@ public class TestCase_005 {
 	public void testcase1()
 	{
 		/**
-		 * @wi.implements HomeAutomation2000/HA-430 It should say facebookkk
+		 * @wi.implements HomeAutomation2000/HA-430 It should say facebook
 		 */
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
