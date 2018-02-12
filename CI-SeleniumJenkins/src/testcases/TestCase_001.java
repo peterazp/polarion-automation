2a5f2bd8538aabdaa8f6f05e8ab1aa5fe9c6fe6d
Peter Asp <peter.aspus@gmail.com>
2018-02-12 18:43:47 +0100

    HA-601 Changing key to Pjotr

--- CI-SeleniumJenkins/src/testcases/TestCase_001.java
+++ CI-SeleniumJenkins/src/testcases/TestCase_001.java
@@ -11,11 +11,11 @@ public class TestCase_001 {
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver  = new ChromeDriver();
 		/**
-		 * @wi.implements HomeAutomation2000/HA-560 Implementing Function A
+		 * @wi.implements HomeAutomation2000/HA-601 Implementing Function D
 		 */
 		driver.get("http://facebook.com");
 		driver.findElementById("email").sendKeys("Hello");
-		driver.findElementById("pass").sendKeys("Arvid");
+		driver.findElementById("pass").sendKeys("Pjotr");
 		driver.quit();
 		
 	}
